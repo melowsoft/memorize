68c25d9f3262850073c50db01fb4ee590964c4f9
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-14 18:18:49 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tony on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
