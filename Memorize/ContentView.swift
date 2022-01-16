be76d01f5996356f296b342de1fecc2da44c9582
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-16 14:05:08 +0100

    first commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = [ "🚂", "🚀", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚐", "🛻", "🚲", "⛵️", "🛳", "🛶", "🚖","🛴", "🦼", "🚆", "🛺", "⛽️"]
+    
+   @State var emojiCount = 4
+    
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { item in
+                    CardView(content: item)
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+        }
+        .foregroundColor(.red)
+    .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var content: String
+   @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape .fill()
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
