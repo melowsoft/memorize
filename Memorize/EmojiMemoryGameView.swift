5365b147a273b853d2f8eece5a94da5e4c7e6cbd
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-28 01:26:32 +0100

    added pie shape

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -33,7 +33,8 @@ struct EmojiMemoryGameView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryGameView(game: game)
+        game.choose(game.cards.first!)
+       return EmojiMemoryGameView(game: game)
             .preferredColorScheme(.dark)
     }
 }
@@ -48,6 +49,8 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: 3)
+                    Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
+                        .padding(5).opacity(0.5)
                     Text(card.content)
                         .font(font(in: geometry.size))
                 } else if card.isMatched {
@@ -67,6 +70,6 @@ struct CardView: View {
     private struct DrawingConstant {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
+        static let fontScale: CGFloat = 0.70
     }
 }
